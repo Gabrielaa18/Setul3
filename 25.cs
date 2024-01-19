022c1107e91f593279ca735d1a239d4acf6529b8
MaghearGabriela <147068414+Gabrielaa18@users.noreply.github.com>
2024-01-19 19:39:44 +0200

    Add files via upload

---
+++ 25.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti primul vector: ");
+            int[] v1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.Write("Introduceti al doilea vector: ");
+            int[] v2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] result = MergeSortedArrays(v1, v2);
+            Console.WriteLine();
+            Console.WriteLine("Vectorul interclasat este:");
+            foreach (int num in result)
+            {
+                Console.Write(num + " ");
+            }
+        }
+        static int[] MergeSortedArrays(int[] v1, int[] v2)
+        {
+            int[] result = new int[v1.Length + v2.Length];
+            int i = 0, j = 0, k = 0;
+            while (i < v1.Length && j < v2.Length)
+            {
+                if (v1[i] < v2[j])
+                {
+                    result[k] = v1[i];
+                    i++;
+                }
+                else
+                {
+                    result[k] = v2[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < v1.Length)
+            {
+                result[k] = v1[i];
+                i++;
+                k++;
+            }
+
+            while (j < v2.Length)
+            {
+                result[k] = v2[j];
+                j++;
+                k++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 28.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti elementele vectorului: ");
+            int[] v = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            QuickSort(v, 0, v.Length - 1);
+
+            Console.WriteLine("Vectorul sortat: " + string.Join(" ", v));
+        }
+        static void QuickSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partitie(array, left, right);
+                QuickSort(array, left, pivotIndex - 1);
+                QuickSort(array, pivotIndex + 1, right);
+            }
+        }
+        static int Partitie(int[] array, int left, int right)
+        {
+            int pivot = array[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (array[j] <= pivot)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+
+            Swap(array, i + 1, right);
+
+            return i + 1;
+        }
+        static void Swap(int[] v, int i, int j)
+        {
+            int temp = v[i];
+            v[i] = v[j];
+            v[j] = temp;
+        }
+    }
+}
+
